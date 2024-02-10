2839b81633c0aec0504c6fb496cc6058596dc73e
s0m31 <admin@nwolfhub.org>
2024-02-10 11:14:44 +0300

    Initial commit

---
+++ src/main/java/org/nwolfhub/unchessedGame/UnchessedGameApplication.java
@@ -0,0 +1,13 @@
+package org.nwolfhub.unchessedGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnchessedGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnchessedGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nwolfhub/unchessedGame/UnchessedGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nwolfhub.unchessedGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnchessedGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
